6f0a37d40dcaaf9a0f9ac79f60ab2d00e9064f27
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-27 20:53:52 +0100

    Update Laberinto.py

--- Laberinto.py
+++ Laberinto.py
@@ -1,9 +1,9 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 estructura = len(muro)
 ultima_coordenada = muro[(estructura - 1)][0]
-laberinto = [[]] * (ultima_coordenada + 1)
-
-print(laberinto)
+laberinto = [[]]
+for p in range(ultima_coordenada):
+    laberinto += [[]]
 
 def comparacion(i, j):
     haymuro = False
@@ -16,11 +16,12 @@ def comparacion(i, j):
         laberinto[i].append(" ")
     return laberinto
 
-def caca():
-        for i in range(ultima_coordenada + 1):
-            for j in range(ultima_coordenada + 1):
-                print(i, j)
-                comparacion(i, j)
+for i in range(ultima_coordenada + 1):
+    for j in range(ultima_coordenada + 1):
+        comparacion(i, j)
 
-comparacion(0, 0)
-print(laberinto)
+laberinto[(len(laberinto) - 1)].pop()
+laberinto[(len(laberinto) - 1)].append("S")
+for z in range(len(laberinto) - 1):
+    print(str(laberinto[z]) +  ",")
+print(laberinto[len(laberinto) - 1])
