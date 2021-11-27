f1e5864b514ec100a553dc6dab03ae789096264c
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-27 10:27:28 +0100

    Update Laberinto.py

--- Laberinto.py
+++ Laberinto.py
@@ -1,3 +1,6 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 laberinto = [[]] * 5
 print(muro[1][1])
+for i in range(5):
+    laberinto[i].append("X")
+print(laberinto)
