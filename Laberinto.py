ef8b28d5fdae4974de0a5918764e9f97a431cefc
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-27 11:53:37 +0100

    Update Laberinto.py

--- Laberinto.py
+++ Laberinto.py
@@ -1,18 +1,22 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 laberinto = [[]] * 5
 
-def construccion(fila, numero):
-    if numero == 0:
-        laberinto[0].append("X")
-    if numero == 1:
-        laberinto[1].append("X")
-    if numero == 2:
-        laberinto[2].append("X")
-    if numero == 3:
-        laberinto[3].append("X")
-    if numero == 4:
-        laberinto[4].append("X")
+def construccion(i, j):
+    if muro[0] == (i, j):
+        laberinto[i].append("X")
+    elif muro[1] == (i, j):
+        laberinto[i].append("X")
+    elif muro[2] == (i, j):
+        laberinto[i].append("X")
+    elif muro[3] == (i, j):
+        laberinto[i].append("X")
+    elif muro[4] == (i, j):
+        laberinto[i].append("X")
+    else:
+        laberinto[i].append(" ")
 
 for i in range(5):
     for j in range(5):
-        
+        construccion(i, j)
+
+print(laberinto)
