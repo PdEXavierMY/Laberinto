f6679f4bf0601629c6c71d42eaa7511208a2d3a8
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-27 18:43:54 +0100

    Update Laberinto.py

--- Laberinto.py
+++ Laberinto.py
@@ -1,22 +1,20 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
-laberinto = [[]] * 5
+estructura = len(muro)
+ultima_coordenada = muro[(estructura - 1)][0]
+laberinto = [[]] * (ultima_coordenada + 1)
+
+print(laberinto)
 
 def construccion(i, j):
-    if muro[0] == (i, j):
-        laberinto[i].append("X")
-    elif muro[1] == (i, j):
-        laberinto[i].append("X")
-    elif muro[2] == (i, j):
-        laberinto[i].append("X")
-    elif muro[3] == (i, j):
-        laberinto[i].append("X")
-    elif muro[4] == (i, j):
-        laberinto[i].append("X")
-    else:
-        laberinto[i].append(" ")
+    for k in muro:
+        if k == (i, j):
+            laberinto[i].append("X")
+        else:
+            laberinto[i].append(" ")
 
 for i in range(5):
     for j in range(5):
         construccion(i, j)
 
 print(laberinto)
+print(estructura)
