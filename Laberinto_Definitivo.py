c3567543850e85c84cef1e2dfbd1d65ddbde5a77
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-29 19:25:00 +0100

    Update Laberinto_Definitivo.py

--- Laberinto_Definitivo.py
+++ Laberinto_Definitivo.py
@@ -37,24 +37,24 @@ for i in range(filas):
             if laberinto[i + 1][j] == " ":
                 laberinto[i][j] = "."
                 movimientos += "Abajo"
-        if laberinto[i - 1][j] == IndexError:
-            pass
-        else: 
-            if laberinto[i - 1][j] == " ":
-                laberinto[i][j] = "."
-                movimientos += "Arriba"
-        if laberinto[i][j + 1] == IndexError:
-            pass
-        else: 
-            if laberinto[i][j + 1] == " ":
-                laberinto[i][j] = "."
-                movimientos += "Derecha"
-        if laberinto[i][j - 1] == IndexError:
-            pass
-        else: 
-            if laberinto[i][j - 1] == " ":
-                laberinto[i][j] = "."
-                movimientos += "Izquierda"
+            if laberinto[i - 1][j] == IndexError:
+                pass
+            else: 
+                if laberinto[i - 1][j] == " ":
+                    laberinto[i][j] = "."
+                    movimientos += "Arriba"
+                if laberinto[i][j + 1] == IndexError:
+                    pass
+                else: 
+                    if laberinto[i][j + 1] == " ":
+                        laberinto[i][j] = "."
+                        movimientos += "Derecha"
+                    if laberinto[i][j - 1] == IndexError:
+                        pass
+                    else: 
+                        if laberinto[i][j - 1] == " ":
+                            laberinto[i][j] = "."
+                            movimientos += "Izquierda"
 
 print(laberinto)
 print(movimientos)
