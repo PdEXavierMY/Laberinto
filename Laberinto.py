fd83f1dd2164092f1fa0cd6bad05571e41d51693
Xavitheforce <javier.miguelez.yague@gmail.com>
2021-11-29 10:18:10 +0100

    Update Laberinto.py

--- Laberinto.py
+++ Laberinto.py
@@ -1,8 +1,9 @@
 muro = ((0,1), (0,2), (0,3), (0,4), (1,1), (2,1), (2,3), (3,3), (4,0), (4,1), (4,2), (4,3))
 estructura = len(muro)
-filas = 5
+filasycolumnas = 5
+#Se define con antelación el número de filas y columnas del laberinto
 laberinto = [[]]
-for p in range(filas - 1):
+for p in range(filasycolumnas - 1):
     laberinto += [[]]
 
 def comparacion(i, j):
@@ -16,11 +17,11 @@ def comparacion(i, j):
         laberinto[i].append(" ")
     return laberinto
 
-for i in range(filas):
-    for j in range(filas):
+for i in range(filasycolumnas):
+    for j in range(filasycolumnas):
         comparacion(i, j)
 
-laberinto[filas - 1][len(laberinto[filas - 1]) - 1] = "S"
+laberinto[filasycolumnas - 1][len(laberinto[filasycolumnas - 1]) - 1] = "S"
 for z in range(len(laberinto) - 1):
     print(str(laberinto[z]) +  ",")
 print(laberinto[len(laberinto) - 1])
